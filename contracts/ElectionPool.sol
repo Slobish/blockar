77bd4d2af14d016ae7bea3ae7028040277ce53ce
Franco <scucchiero@gmail.com>
2018-12-31 13:45:02 -0300

    Improvements in contract

--- contracts/ElectionPool.sol
+++ contracts/ElectionPool.sol
@@ -9,9 +9,6 @@ import "./Election.sol";
 
 contract ElectionPool is Ownable {
     
-    address[] moderators;
-    uint8 required_confirmations = 0;
-    
     struct ElectionRegistration {
         bool registered;
         bool accepted;
@@ -20,17 +17,45 @@ contract ElectionPool is Ownable {
         uint256 index;
     }
 
+    struct Moderator {
+        bool isRegistered;
+        address moderatorAddress;
+        uint256 timestampRegistration;
+        uint8 weight;
+        uint256 index;
+    }
+
+    struct Votes {
+        uint16 balance;
+        uint16 votes;
+    }
+
     mapping (bytes32 => mapping (address => int8)) public moderators_votes;
     mapping (bytes32 => ElectionRegistration) public registered_elections;
+    mapping (address => Moderator) public moderators;
+    
     bytes32[] iterable_elections;
+    address[] iterable_moderators;
+    uint8 required_confirmations = 0;
 
     constructor(address[] initial_moderators, uint8 initial_required_confirmations) public {
         require (len(initial_moderators) > 0, "There must be at least 1 initial moderator.");
         require (initial_required_confirmations > 0, "There must be at least 1 initial required confirmation.");
 
+        for(int i = 0; i < initial_moderators.length; i++) {
+            uint256 current_index = iterable_moderators.push(initial_moderators).sub(1);
+            Moderator memory newModerator = Moderator({
+                moderatorAddress: initial_moderators[i],
+                timestampRegistration: now,
+                index: current_index
+            });
+            moderators[initial_moderators[i]] = newModerator;
+        }
+
+        required_confirmations = initial_required_confirmations;
     }
     
-
+    
     /*
     *   Duration in seconds
     */
@@ -47,8 +72,23 @@ contract ElectionPool is Ownable {
             registered_timestamp: now,
             index: index
         });
-
         registered_tokens[token_key] = newElection;
     }
 
+    /*
+    *   Voting options: 
+    *    1 : Yes
+    *   -1 : No
+    */
+    function voteElection(string election_reason, int8 vote) public onlyModerators {
+        require(vote == 1 || vote == -1, "Only 1 or -1 are allowed");
+        require(iterable_elections[keccak256(election_reason)] != 0, "Election doesn't exists");
+        moderators_votes[keccak256(election_reason)] = vote;
+
+    }
+
+    modifier onlyModerators() {
+        require(moderators[msg.sender], "You are not registered as a moderator");
+        _;
+    }
 }
